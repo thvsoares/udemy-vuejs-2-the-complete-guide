2ac9d995e8aaa95418bd0f1eacbc0d534cd4789d
Thiago Soares <thiagomullet@gmail.com>
2019-10-02 20:59:07 -0300

    Part2 - whatch handler for result

--- Exercise3/app.js
+++ Exercise3/app.js
@@ -7,5 +7,11 @@ new Vue({
         result() {
             return this.value != 37 ? 'Not there yet!' : 'Considerate done!'
         }
+    },
+    watch: {
+        result() {
+            const vm = this;
+            setTimeout(() => vm.value = 0, 5000)
+        }
     }
 });
